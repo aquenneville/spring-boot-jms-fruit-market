823907bb3f1e55bd8d9c025514418471a2ce2e60
aquenneville <alainquenneville@gmail.com>
2018-08-30 22:43:37 +0100

    initial common code

---
+++ fruit-market-common/src/main/java/github/aq/fruitmarket/common/FruitName.java
@@ -0,0 +1,5 @@
+package github.aq.fruitmarket.common;
+
+public enum FruitName {
+	APPLE, ORANGE, BANANA;
+}
---
+++ fruit-market-common/src/main/java/github/aq/fruitmarket/common/IFruit.java
@@ -0,0 +1,10 @@
+package github.aq.fruitmarket.common;
+
+import java.math.BigDecimal;
+
+public interface IFruit {
+
+	public String getName();
+	public BigDecimal getPrice();
+	public BigDecimal getQuantity();
+}
